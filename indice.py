aecabb56e71e40a3223ee75319f3e75234eaa7c2
Murilo Guizelin Motta <66173617+MottaMurilo@users.noreply.github.com>
2022-01-06 16:30:10 -0400

    Add files via upload

---
+++ indice.py
@@ -0,0 +1,9 @@
+# Indices em Python
+
+texto = 'Murilo Motta'
+       '[012345678910]' # <-- índices
+
+print(texto[3]) # Output: 'i'
+print(texto[-3]) # Output: 't'
+print(texto[:3]) # Output: 'muri'
+print(texto[7:10]) # Output: 'motta'
