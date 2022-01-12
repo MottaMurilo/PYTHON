3807f501961005c22d7b49dd1f4e2d77cc13cef5
Murilo Guizelin Motta <66173617+MottaMurilo@users.noreply.github.com>
2022-01-12 18:36:36 -0300

    Update salario.py

--- salario.py
+++ salario.py
@@ -1,8 +1,11 @@
-salario = int(input('Quanto você ganha por mês?'))
-imposto_renda = salario*0.11
-inss = salario*0.08
-sindicato = salario*0.05
+salario_bruto = int(input('Quanto você ganha por mês?'))
 
-salario_liquido = salario-inss-imposto_renda-sindicato
+imposto_renda = salario*0.11 #11% de  IR
 
-print(f'Seu salário bruto é {salario}, com os descontos sobra {salario_liquido}')
+inss = salario*0.08 #8% do INSS
+
+sindicato = salario*0.05 #5% do sindicato
+
+salario_liquido = salario-inss - imposto_renda - sindicato
+
+print(f'Seu salário bruto é {salario_bruto}, com os descontos fica {salario_liquido}!')
