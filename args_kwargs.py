faa8fd9f605e582d37b9df7321bd5dd8af2dd50f
Murilo Guizelin Motta <66173617+MottaMurilo@users.noreply.github.com>
2022-01-18 22:59:35 -0300

    Update args_kwargs.py

--- args_kwargs.py
+++ args_kwargs.py
@@ -1,3 +1,9 @@
 def mestre(funcao, *args, **kwargs):
     return funcao(*args, **kwargs)
 
+def fala_oi(nome):
+    return f'Oi {nome}'
+
+def saudacao(nome, saudacao):
+    return f'{saudacao} {nome}'
+
